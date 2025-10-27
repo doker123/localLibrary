4c43cf88e9b39b2114c6379770a55dcc615cfa46
Doker123 <dodo2larin@gmail.com>
2025-10-27 19:42:54 +0700

    Написал пагинацию списков книг и авторов

--- catalog/views.py
+++ catalog/views.py
@@ -26,14 +26,14 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
-    paginate_by = 10
+    paginate_by = 2
 
 class BookDetailView(generic.DetailView):
     model = Book
 
 class AuthorListView(generic.ListView):
     model = Author
-    paginate_by = 10
+    paginate_by = 2
 
 class AuthorDetailView(generic.DetailView):
     model = Author
