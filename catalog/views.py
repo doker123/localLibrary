eb93884592694b917cabb919e73c9e7fa02b7991
Doker123 <dodo2larin@gmail.com>
2025-10-27 19:14:02 +0700

    Написал Представления для Авторов и Книг и пути к ним

--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-
-from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+from .models import Book, Author, BookInstance
 
 
 # Create your views here.
 def index(request):
-    """View function for home page of site."""
-    # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
     # Available copies of books
@@ -14,8 +12,28 @@ def index(request):
         status__exact='a').count()
     num_authors = Author.objects.count()
 
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+
     return render(
-        request,'index.html',
+        request,
+        'index.html',
         context={'num_books': num_books, 'num_instances': num_instances,
-        'num_instances_available': num_instances_available, 'num_authors': num_authors},
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors,
+                 'num_visits': num_visits},
     )
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
